915bf582d1ffdb5f1dd67d7aad2805d6cf77ef02
sheng--qing <1406707496@qq.com>
2022-04-13 15:29:50 +0800

    发现一个小bug

--- 2.Stack/LinkStack.cpp
+++ 2.Stack/LinkStack.cpp
@@ -10,7 +10,6 @@ using namespace std;
 #define OK 1
 #define ERROR 0
 #define INFEASIBLE -1//不可行
-#define OVERFLOW -2 //溢出
 
 typedef int Status;       //返回状态
 typedef char SElemType;    //栈元素类型
@@ -99,5 +98,6 @@ Status GetTop(LinkStack S){
     if(S != NULL){
         return S->data;
     }
+    return OK;
 }
 
